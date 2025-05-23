e2cd4539221ad8e0a388e5488d451476759f1eef
AgustinaWaigel <soledad.waigel@uap.edu.ar>
2025-05-23 09:09:16 -0300

    g

--- utils/sendPush.js
+++ utils/sendPush.js
@@ -1,45 +1,36 @@
 const admin = require("firebase-admin");
 
-// Detecta si estamos en producción o desarrollo
 let firebaseConfig;
 
 if (process.env.FIREBASE_CONFIG_JSON) {
-  // 🌐 Producción (Render)
   firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG_JSON);
-
-  // 🔧 Corrige el formato del private_key
   if (firebaseConfig.private_key) {
     firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
   }
-
   console.log("✅ Usando configuración desde FIREBASE_CONFIG_JSON");
 } else {
-  // 🧪 Local
   firebaseConfig = require("../firebase-key.json");
   console.log("✅ Usando configuración local desde firebase-key.json");
 }
 
-// Inicializar Firebase Admin
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(firebaseConfig),
   });
 }
 
-// Función para enviar notificación
 const sendPush = async (token, title, body, data = {}) => {
   const message = {
-  notification: {
-    title: "📢 Nuevo evento misionero",
-    body: "Te esperamos este sábado a las 16 hs en la parroquia.",
-  },
-  token,
-  data: {
-    link: "https://iam-animadores-client.vercel.app/",
-    ...data,
-  },
-};
-
+    notification: {
+      title,
+      body,
+    },
+    token,
+    data: {
+      link: "https://iam-animadores-client.vercel.app/",
+      ...data,
+    },
+  };
 
   try {
     const response = await admin.messaging().send(message);
